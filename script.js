b6eb446f9997f532f9bd84453640ca0d05a20af0
awexli <awexli@mail.sfsu.edu>
2020-02-03 00:32:32 -0800

    test prettier

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const GameBoard = (() => {
-	const _boardID = [
-		['zero', 'one', 'two'],
-		['three', 'four', 'five'],
-		['six', 'seven', 'eight']
-	];
+  const _boardID = [
+    ["zero", "one", "two"],
+    ["three", "four", "five"],
+    ["six", "seven", "eight"]
+  ];
 
-	const _boardSet = {
-		zero: 2,
-		one: 2,
-		two: 2,
-		three: 2,
-		four: 2,
-		five: 2,
-		six: 2,
-		seven: 2,
-		eight: 2
-	};
+  const _boardSet = {
+    zero: 2,
+    one: 2,
+    two: 2,
+    three: 2,
+    four: 2,
+    five: 2,
+    six: 2,
+    seven: 2,
+    eight: 2
+  };
 
-	const getBoardID = () => {
-		return _boardID;
-	};
+  const getBoardID = () => {
+    return _boardID;
+  };
 
-	const renderBoard = () => {
-		const divBoard = document.querySelector('#board');
-		const boardTemplate = `
+  const renderBoard = () => {
+    const divBoard = document.querySelector("#board");
+    const boardTemplate = `
         <div id=row-1 class="row">
             <div id="zero" class="square"></div>
             <div id="one" class="square"></div>
@@ -40,33 +40,36 @@ const GameBoard = (() => {
             <div id="eight" class="square"></div>
         </div>
         `;
-		divBoard.innerHTML = boardTemplate;
-	};
+    divBoard.innerHTML = boardTemplate;
+  };
 
-	const markBoard = id => {
-		const squareID = document.querySelector(`#${id}`);
-		if (_boardSet[id] && _boardSet[id] != 1) {
-			squareID.innerText = 'x';
-			_boardSet[id]--;
-			console.log(_boardSet);
-		} else {
-			console.log('false');
-		}
-	};
+  const markBoard = id => {
+    const squareID = document.querySelector(`#${id}`);
+    if (_boardSet[id] && _boardSet[id] != 1) {
+      // if player x turn
+      squareID.innerText = "x";
+      // if player o turn
 
-	return {
-		getBoardID,
-		renderBoard,
-		markBoard
-	};
+      _boardSet[id]--;
+      console.log(_boardSet);
+    } else {
+      console.log("false");
+    }
+  };
+
+  return {
+    getBoardID,
+    renderBoard,
+    markBoard
+  };
 })();
 
 GameBoard.renderBoard();
 
-document.addEventListener('click', e => {
-	if (e.target.className == 'square') {
-		GameBoard.markBoard(e.target.id);
-	}
+document.addEventListener("click", e => {
+  if (e.target.className == "square") {
+    GameBoard.markBoard(e.target.id);
+  }
 });
 
 // check for win condition on the 5th turn
